af952c877471360dbe5720c72288303bd814ba76
juxx8 <linwujun2002@gmail.com>
2018-10-03 13:06:12 -0700

    testing grid

--- SHHS/SHHS/MainPage.xaml.cs
+++ SHHS/SHHS/MainPage.xaml.cs
@@ -10,8 +10,49 @@ namespace SHHS
     public partial class MainPage : ContentPage
     {
         public MainPage()
-        {
+        {    
             InitializeComponent();
+
+
+
+            int column = 0;
+            int row = 1;
+            
+            for (int date =1; date<= 31; date++)
+            {
+                var label = new Label { Text= $"{date}"};
+                Calendar.Children.Add(label,column,row);
+
+                if (date % 7 == 0)
+                {
+                    column = 0;
+                    row++;
+                }
+                else {
+
+                    column++;
+
+                }
+
+
+            }
+
+            RelativeView.Children.Add(new Button { Text = "Announcement1", BorderColor = Color.White, IsEnabled = false, CornerRadius = 35, BorderWidth = 2 },
+                                                                            Constraint.RelativeToParent(parent=>parent.X+35),
+                                                                            Constraint.RelativeToParent(parent=>parent.Width*0.8),
+                                                                            Constraint.RelativeToParent(parent=>monthLabel.Y+350),
+                                                                            Constraint.RelativeToParent(parent=>monthLabel.Height*2));
+            RelativeView.Children.Add(new Button { Text = "Announcement2", BorderColor = Color.White, CornerRadius = 35, BorderWidth = 2, IsEnabled = false },
+                                                                            Constraint.RelativeToParent(parent => parent.X + 35),
+                                                                            Constraint.RelativeToParent(parent => parent.Width * 0.8),
+                                                                            Constraint.RelativeToParent(parent => monthLabel.Y + 500),
+                                                                            Constraint.RelativeToParent(parent => monthLabel.Height * 2));
+
         }
+        
+        }
+
+
+
     }
-}
+
